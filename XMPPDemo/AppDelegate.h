70f2927026b17265cb4f92598e209789e1c0a7d7
JackyWang2014 <339847016@qq.com>
2015-12-07 09:51:04 +0800

    first

---
+++ XMPPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPDemo
+//
+//  Created by 王亓泰 on 15/12/7.
+//  Copyright (c) 2015年 QianFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPDemo
+//
+//  Created by 王亓泰 on 15/12/7.
+//  Copyright (c) 2015年 QianFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
