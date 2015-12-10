ab8ba6c181a25378983d408d6ff8b776727ecbd4
JackyWang2014 <339847016@qq.com>
2015-12-10 15:45:18 +0800

    Setting the register view

---
+++ XMPPDemo/Classes/Login/RegisterViewController.m
@@ -0,0 +1,65 @@
+//
+//  RegisterViewController.m
+//  XMPPDemo
+//
+//  Created by 王亓泰 on 15/12/10.
+//  Copyright (c) 2015年 QianFeng. All rights reserved.
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftContraint;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *rightContraint;
+@property (weak, nonatomic) IBOutlet UITextField *userField;
+@property (weak, nonatomic) IBOutlet UITextField *pwdField;
+- (IBAction)registerBtnClick:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+
+@end
+
+@implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    // 判断当前设备的类型 改变当前左右两边约束的距离
+    self.title = @"注册";
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
+        self.leftContraint.constant = 10;
+        self.rightContraint.constant = 10;
+    }
+    
+    // 设置textField背景
+    
+    //    self.userField.background = [UIImage imageNamed:@"operationbox_text"];
+    self.userField.background = [UIImage stretchedImageWithName:@"operationbox_text"];
+    self.pwdField.background = [UIImage stretchedImageWithName:@"operationbox_text"];
+    [self.registerBtn setResizedN_BG:@"fts_green_btn" H_BG:@"fts_green_btn_HL"];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)registerBtnClick:(id)sender {
+}
+
+- (IBAction)cancelBtnClick:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
